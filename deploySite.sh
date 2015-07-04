ea5845a35dd6accf04c07f11806413667ff4eb94
Q-Mart <qumarthjash@gmail.com>
2015-07-04 14:05:20 -0400

    Added git pulling to script

--- deploySite.sh
+++ deploySite.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 #################
-# bash script to deploy git repository to server
+# bash script to synchronise and deploy git repository to server
 
 # Author: Qumarth Jash (github: Q-Mart)
 ################
 
+echo "Pulling repository"
+git pull
+echo "Pulling complete"
+
+echo "Removing old site"
 sudo rm -r /var/www/qumarth.me/*
+
+echo "Deploying update"
 sudo cp -r public_html /var/www/qumarth.me/
