641c536f10fec8c9ff2c84097855c70a5a8131b8
Q-Mart <qumarth.jash@ury.org.uk>
2015-09-14 14:41:11 +0100

    Switched to CDN and removed styling

--- html/index.php
+++ html/index.php
@@ -7,15 +7,6 @@
     <h1 id="name" class="col-md-offset-5 col-md-6" id="title">Hi, I'm Qumarth</h1>
     <h2 class="col-md-offset-3 col-md-7" id="content"> <span class="typing-text"></span></h2>
 
-    <style id="blurring-name" type="text/css">
-      #name{
-        -webkit-filter: blur(0px);
-        -moz-filter: blur(0px);
-        -o-filter: blur(0px);
-        -ms-filter: blur(0px);
-        filter: blur(0px);
-      }
-    </style>
   <!-- Custom Stylesheet -->
   <link href="css/index.css" rel="stylesheet">
 
--- html/templates/head.php
+++ html/templates/head.php
@@ -8,12 +8,12 @@
     <title>Qumarth Jash</title>
 
     <!-- Bootstrap -->
-    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
 
 
